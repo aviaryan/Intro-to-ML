8f66f13acd6d193995b12f3769c364a7a24bc589
Avi Aryan <avi.aryan123@gmail.com>
2017-02-12 22:32:30 +0530

    chore: use C value in svm

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -31,11 +31,11 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 
 # make set small
-features_train = features_train[:len(features_train)/100] 
-labels_train = labels_train[:len(labels_train)/100] 
+# features_train = features_train[:len(features_train)/100] 
+# labels_train = labels_train[:len(labels_train)/100] 
 
 # svm
-clf = SVC(kernel="rbf", cache_size=7000)
+clf = SVC(kernel="rbf", cache_size=7000, C=10000.0)
 
 t0 = time()
 
