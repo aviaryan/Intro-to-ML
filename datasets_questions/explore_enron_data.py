b94b302ca9f373bc94f4c8b5b936ede9e1bd1130
Avi Aryan <avi.aryan123@gmail.com>
2017-02-16 22:09:33 +0530

    feat: complete “explore your dataset” section

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -18,16 +18,22 @@
 import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
-poi_ct = 0
+v1 = 0
+sal_ct = 0
 
 for i in enron_data:
-	if enron_data[i]['poi'] == 1:
-		poi_ct += 1
+	# if enron_data[i]['poi'] == 1:
+	# 	poi_ct += 1
 	# if i == 'PRENTICE JAMES':
 	# 	print enron_data[i]
 	# if i == 'COLWELL WESLEY':
 	# 	print enron_data[i]
-	if i == 'SKILLING JEFFREY K':
-		print enron_data[i]
+	# for word in ['LAY', 'SKILLING', 'FASTOW']:
+	# 	if i.find(word) > -1:
+	# 		print enron_data[i]
+	if enron_data[i]['salary'] != 'NaN':
+		sal_ct += 1
+	if enron_data[i]['email_address'] != 'NaN':
+		v1 += 1
 
-print poi_ct
+print sal_ct, v1
